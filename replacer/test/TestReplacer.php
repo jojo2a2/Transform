9752f5f6b6c4531aeeb267cbf3e11de4275c68fe
jojo2a2 <49288102+jojo2a2@users.noreply.github.com>
2019-04-05 13:12:50 +0200

    Update TestReplacer.php

--- replacer/test/TestReplacer.php
+++ replacer/test/TestReplacer.php
@@ -2,17 +2,30 @@
 /**
  * Created by PhpStorm.
  * User: jozef.chladek
- * Date: 2019-04-04
- * Time: 19:29
+ * Date: 2019-04-05
+ * Time: 12:13
  */
 
-
+use Replacer\ReplacerController;
 
 class TestReplacer extends PHPUnit\Framework\TestCase
 {
-    public function testControllIfNumbersAreReplaced()
+    public function testIfNumbersAreReplaced(): void
     {
-        $test = new \Chatter\MVC\controller\ReplacerController();
+        $test = new \Replacer\ReplacerController();
         $this->assertEquals('L', $test->replace(50));
+    }
+
+    public function testIfInputIsNumeric(): void
+    {
+        $test = new \Replacer\ReplacerController();
+        $this->assertIsNumeric('int', 'Is not Numeric');
+    }
+
+    public function testIfInputIsEmpty(): void
+    {
+        $num = null;
+        $test = new \Replacer\ReplacerController();
+        $this->assertEmpty($num);
     }
 }
